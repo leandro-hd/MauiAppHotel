ef1689557c79d5012b830679682fc13b12fef61f
Leandro Illan Higa Dias <leandro.dias@tecnospeed.com.br>
2024-11-03 21:14:01 -0300

    style: AGENDA-14 - Adicionar fonte Poppins Regular (400)

--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -16,6 +16,7 @@ namespace MauiAppHotel
                     fonts.AddFont("Kalam-Bold.ttf", "KalamBold");
                     fonts.AddFont("Kalam-Light.ttf", "KalamLight");
                     fonts.AddFont("Kalam-Regular.ttf", "Kalam");
+                    fonts.AddFont("Poppins-Regular.ttf", "PoppinsRegular");
                 });
 
 #if DEBUG
